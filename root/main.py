ce1dba0ec637f5a3763be0b7354cab6d99e2a8a4
adith-p <adithprakash008@gmail.com>
2024-02-21 17:16:38 +0530

    Added the Truncate and Drop endpoints

--- root/main.py
+++ root/main.py
@@ -22,7 +22,7 @@ def create_table(request:TableSchema,db: Session = Depends(get_db)):
     db.execute(text(sql))
     db.commit()
     
-    return {'message':'message'}
+    return {'message': f'table {request.tbl_name} created successfuly'}
 
 @app.get('/fetch-data/{table_name}')
 def fetch_table(table_name:str, limit: int = 10, offset: int = 1,db: Session = Depends(get_db)):
@@ -36,4 +36,39 @@ def fetch_table(table_name:str, limit: int = 10, offset: int = 1,db: Session = D
         return JSONResponse(serialized_data)
     
     return {'message': f' the table {table_name} does not exist','code':status.HTTP_404_NOT_FOUND}
+
+
+@app.delete('/{table_name}/truncate')
+def truncate_table(table_name:str,db: Session = Depends(get_db)):
     
+    if does_table_exist(table_name):
+        try:
+            sql = text(f' TRUNCATE TABLE {table_name} ')
+            db.execute(sql)
+            db.commit()
+            return {'message': f'table {table_name} truncated successfuly'}
+        
+        except Exception as e:
+            db.rollback()
+            return {'message': f'error while truncating table {table_name}','code':status.HTTP_400_BAD_REQUEST}
+            
+    return {'message': f' the table {table_name} does not exist','code':status.HTTP_404_NOT_FOUND}
+    
+@app.delete('/{table_name}/delete')
+def delete_table(table_name:str,db:Session = Depends(get_db)):
+    
+    if does_table_exist(table_name):
+        try:
+            sql = text(f' DROP TABLE {table_name}')
+            db.execute(sql)
+            db.commit()
+            return {'message': f'table {table_name} deleted successfuly','code':status.HTTP_200_OK}
+        except Exception as e:
+            db.rollback()
+            
+            return {'message': f'error while deleting table {table_name}','code':status.HTTP_400_BAD_REQUEST}
+    return {'message': f' the table {table_name} does not exist','code':status.HTTP_404_NOT_FOUND}
+        
+        
+        
+        
