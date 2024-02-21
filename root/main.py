bf6c8c19a17307f66d83e277d30005576843031d
adith-p <adithprakash008@gmail.com>
2024-02-21 16:44:23 +0530

    Added the endpoint to fetch all table data including pagination

--- root/main.py
+++ root/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,status
+from fastapi.responses import JSONResponse
+
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from databases.database import get_db
@@ -6,16 +8,10 @@ from databases.database import get_db
 
 from schema.schema import TableSchema
 from utils.sql_dtypes import create_sql
+from utils.dynamic_serializers import does_table_exist,dynamic_serialization
 
 app = FastAPI()
 
-# @app.get('/')
-# def get_db(db: Session = Depends(get_db)):
-#     sql = text(' SELECT * FROM demo')
-#     result = db.execute(sql)
-#     print(result.all())
-    
-#     return {'message':'message'}
 
 @app.post('/create-table')
 def create_table(request:TableSchema,db: Session = Depends(get_db)):
@@ -27,3 +23,17 @@ def create_table(request:TableSchema,db: Session = Depends(get_db)):
     db.commit()
     
     return {'message':'message'}
+
+@app.get('/fetch-data/{table_name}')
+def fetch_table(table_name:str, limit: int = 10, offset: int = 1,db: Session = Depends(get_db)):
+    
+    
+    if does_table_exist(table_name): # check if the table exist
+        sql = db.execute(text(f' SELECT * FROM {table_name} LIMIT :limit OFFSET :offset'),{'limit':limit,'offset':offset})
+        result = sql.all()    
+        serialized_data = dynamic_serialization(sql, result)
+        
+        return JSONResponse(serialized_data)
+    
+    return {'message': f' the table {table_name} does not exist','code':status.HTTP_404_NOT_FOUND}
+    
