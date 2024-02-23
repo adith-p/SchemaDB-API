1e957ac748104540017662ced0006ba82cb991a0
adith-p <adithprakash008@gmail.com>
2024-02-23 13:41:59 +0530

    added api end-point for pushing data into table

--- root/main.py
+++ root/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI,Depends,status
 from fastapi.responses import JSONResponse
 
+
 from sqlalchemy.orm import Session
 from sqlalchemy import text
+from sqlalchemy.exc import IntegrityError,ProgrammingError
 from databases.database import get_db
 
 
-from schema.schema import TableSchema
+from schema.schema import TableSchema,Insert_data
 from utils.sql_dtypes import create_sql
 from utils.dynamic_serializers import does_table_exist,dynamic_serialization
 
@@ -27,9 +29,9 @@ def create_table(request:TableSchema,db: Session = Depends(get_db)):
 @app.get('/fetch-data/{table_name}')
 def fetch_table(table_name:str, limit: int = 10, offset: int = 1,db: Session = Depends(get_db)):
     
-    
+
     if does_table_exist(table_name): # check if the table exist
-        sql = db.execute(text(f' SELECT * FROM {table_name} LIMIT :limit OFFSET :offset'),{'limit':limit,'offset':offset})
+        sql = db.execute(text(f' SELECT * FROM "db.{table_name}" LIMIT :limit OFFSET :offset'),{'limit':limit,'offset':offset})
         result = sql.all()    
         serialized_data = dynamic_serialization(sql, result)
         
@@ -46,11 +48,11 @@ def truncate_table(table_name:str,db: Session = Depends(get_db)):
             sql = text(f' TRUNCATE TABLE {table_name} ')
             db.execute(sql)
             db.commit()
-            return {'message': f'table {table_name} truncated successfuly'}
+            return {'message': f'table {table_name} truncated successfuly','code':status.HTTP_200_OK}
         
-        except Exception as e:
+        except (ProgrammingError, IntegrityError) as e:
             db.rollback()
-            return {'message': f'error while truncating table {table_name}','code':status.HTTP_400_BAD_REQUEST}
+            return {'message': f'error while truncating table {table_name}','code':status.HTTP_400_BAD_REQUEST,'error':str(e)}
             
     return {'message': f' the table {table_name} does not exist','code':status.HTTP_404_NOT_FOUND}
     
@@ -63,12 +65,31 @@ def delete_table(table_name:str,db:Session = Depends(get_db)):
             db.execute(sql)
             db.commit()
             return {'message': f'table {table_name} deleted successfuly','code':status.HTTP_200_OK}
-        except Exception as e:
+        except (ProgrammingError, IntegrityError) as e:
             db.rollback()
             
-            return {'message': f'error while deleting table {table_name}','code':status.HTTP_400_BAD_REQUEST}
+            return {'message': f'error while deleting table {table_name}','code':status.HTTP_400_BAD_REQUEST,'error':str(e)}
     return {'message': f' the table {table_name} does not exist','code':status.HTTP_404_NOT_FOUND}
         
+# ability to added data into the table
+@app.put('/{table_name}')
+async def insert_data(table_name:str,insert_item:Insert_data,db: Session = Depends(get_db)):
+
+    cols = [i for i in insert_item.items.keys()]
+    values = [i for i in insert_item.items.values()]
         
+    if does_table_exist(table_name):
         
-        
+        sql = f"INSERT INTO {table_name} ("
+        sql += ", ".join(cols)
+        sql += ") VALUES ("
+        sql += ", ".join([f"'{i}'" if i not in ['default', 'current_timestamp','null'] else i for i in values])
+        sql += ")"
+        try:
+            db.execute(text(sql))
+            db.commit()
+        except (ProgrammingError, IntegrityError) as e:
+            db.rollback()
+            return {'message': f'error while inserting data into table {table_name}', 'code': status.HTTP_400_BAD_REQUEST, 'error': str(e)}
+
+        return {"message": "Data inserted successfully"}
