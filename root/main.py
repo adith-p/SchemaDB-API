3c3be7c4e317321e73833740b239ae2938b213cc
adith-p <adithprakash008@gmail.com>
2024-03-02 14:54:31 +0530

    Added: bulk input feature and merged with single input endpoint

--- root/main.py
+++ root/main.py
@@ -8,8 +8,8 @@ from sqlalchemy.exc import IntegrityError,ProgrammingError
 from databases.database import get_db
 
 
-from schema.schema import TableSchema,Insert_data,Schema_data,Search_data
-from utils.sql_query import create_sql,search_sql
+from schema.schema import TableSchema,Insert_data,Schema_data,Search_data,Bulk_insert
+from utils.sql_query import create_sql,search_sql,bulk_create
 from utils.db_utils import does_table_exist,dynamic_serialization,table_schema,schema_serialization
 
 app = FastAPI()
@@ -26,8 +26,6 @@ def create_table(request:TableSchema,db: Session = Depends(get_db)):
     
     return {'message': f'table {request.tbl_name} created successfuly'}
 
-
-
 @app.get('/fetch-data/{table_name}')
 def fetch_table(table_name:str, limit: int = 10, offset: int = 0,db: Session = Depends(get_db)):
     
@@ -43,7 +41,7 @@ def fetch_table(table_name:str, limit: int = 10, offset: int = 0,db: Session = D
     return {'message': f' the table {table_name} does not exist','code':status.HTTP_404_NOT_FOUND}
 
 @app.get('/{table_name}/schema')
-def get_schema(table_name:str,db: Session = Depends(get_db)):
+def get_schema(table_name:str):
     table_s = table_schema(table_name)
     try:
         schema_data = schema_serialization(table_s)
@@ -105,23 +103,18 @@ def delete_table(table_name:str,db:Session = Depends(get_db)):
         
 # ability to added data into the table
 @app.put('/{table_name}')
-async def insert_data(table_name:str,insert_item:Insert_data,db: Session = Depends(get_db)):
-
-    cols = [i for i in insert_item.items.keys()]
-    values = [i for i in insert_item.items.values()]
-        
+async def insert_data(table_name:str,insert_item:Bulk_insert, db: Session = Depends(get_db)):
+    
     if does_table_exist(table_name):
-        
-        sql = f"INSERT INTO {table_name} ("
-        sql += ", ".join(cols)
-        sql += ") VALUES ("
-        sql += ", ".join([f"'{i}'" if i not in ['default', 'current_timestamp','null'] else i for i in values])
-        sql += ")"
+        sql = bulk_create(table_name,insert_item)
+        # print(sql)
         try:
-            db.execute(text(sql))
+            query = db.execute(text(sql))
             db.commit()
+            return dynamic_serialization(query,query.all())
         except (ProgrammingError, IntegrityError) as e:
             db.rollback()
             return {'message': f'error while inserting data into table {table_name}', 'code': status.HTTP_400_BAD_REQUEST, 'error': str(e)}
-
-        return {"message": "Data inserted successfully"}
+        
+        
+        
